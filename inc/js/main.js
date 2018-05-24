c7c9242c6004a924f9a990c52326773d2e8001ae
devarkaam <arkaamreg@gmail.com>
2018-05-24 14:47:10 +0200

    index.html, main.js, style.css start

--- inc/js/main.js
+++ inc/js/main.js
@@ -5,12 +5,11 @@ function main() {
     skillset.fadeIn(1000);
     $('.projects').hide();
     $('.projects-button').on('click', function() {
-        //$('.projects').show();
-        //$('.projects').toggle();
-        //$(this).next().toggle();
         $(this).next().slideToggle(600);
         $(this).toggleClass('active');
         $(this).text('Project Viewed');
+        console.log(`El factorial de 5 es = ${factorial(5)}`);
+        title();
     })
 }
 
@@ -22,27 +21,6 @@ function title() {
 }
 
 function factorial(num) {
-    // var fac=1;
-    // if (num === 0 || num === 1) {
-    //     return 1;
-    // } else {
-    //     for (var i = 1; i <= num; i++) {
-    //         fac *= i;
-    //     }
-    //     return fac;
-    // }
-
-    // if (num === 0 || num === 1) {
-    //     return 1;
-    // } else {
-    //     var cont = 1;
-    //     do {
-    //         fac *= cont;
-    //         cont++;
-    //     } while (cont<=num);
-    //     return fac;
-    // }
-
     if (num === 0 || num === 1) {
         return 1;
     }
@@ -50,7 +28,5 @@ function factorial(num) {
     return num * factorial(num-1)
 }
 
-console.log(`El factorial de 5 es = ${factorial(5)}`);
-
 $(document).ready(main);
-//setInterval(title, 1000)    
+//setInterval(title, 1000);
